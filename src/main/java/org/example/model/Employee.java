cd64397258a2775fdb6310b4f699a10d545aaf83
Hybusa <alkouzne007@gmail.com>
2023-03-17 09:58:57 +0300

    done

---
+++ src/main/java/org/example/model/Employee.java
@@ -0,0 +1,21 @@
+package org.example.model;
+
+public class Employee {
+
+    private final String firstName;
+    private final String lastName;
+
+    public Employee(String firstName, String lastName) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
+
+    @Override
+    public String toString() {
+        return "Employee{" +
+                "firstName='" + firstName + '\'' +
+                ", lastName='" + lastName + '\'' +
+                '}';
+    }
+}
+
